c12673294e13618b4a0b0c776faa65e8cbc4062c
Shupeng Xue <dspxue@gmail.com>
2022-09-16 19:34:10 +1000

    fix avg overflow

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ fn draw_pixel(mut image: bmp::Image) -> bmp::Image {
     return image;
 }
 
-fn draw_left_top_to_right_bottom_diagonal(mut image: bmp::Image, x_from: u32, y_from: u32, x_to: u32, y_to: u32) -> bmp::Image {
+fn draw_left_top_to_right_bottom_diagonal(
+    mut image: bmp::Image,
+    x_from: u32,
+    y_from: u32,
+    x_to: u32,
+    y_to: u32,
+) -> bmp::Image {
     if image.get_width() != image.get_height() {
         panic!("canvas not square");
     }
@@ -21,7 +27,13 @@ fn draw_left_top_to_right_bottom_diagonal(mut image: bmp::Image, x_from: u32, y_
     return image;
 }
 
-fn draw_left_bottom_to_right_top_diagonal(mut image: bmp::Image, x_from: u32, y_from: u32, x_to: u32, y_to: u32) -> bmp::Image {
+fn draw_left_bottom_to_right_top_diagonal(
+    mut image: bmp::Image,
+    x_from: u32,
+    y_from: u32,
+    x_to: u32,
+    y_to: u32,
+) -> bmp::Image {
     if image.get_width() != image.get_height() {
         panic!("canvas not square");
     }
@@ -35,10 +47,10 @@ fn draw_left_bottom_to_right_top_diagonal(mut image: bmp::Image, x_from: u32, y_
     return image;
 }
 
-fn draw_diagonal(mut image: bmp::Image) -> bmp::Image {
+fn draw_diagonal(image: bmp::Image) -> bmp::Image {
     let width = image.get_width();
     let height = image.get_height();
-    return draw_left_top_to_right_bottom_diagonal(image, 0, 0, width - 1, height - 1);
+    draw_left_top_to_right_bottom_diagonal(image, 0, 0, width - 1, height - 1)
 }
 
 fn draw_x(mut image: bmp::Image) -> bmp::Image {
@@ -49,8 +61,13 @@ fn draw_x(mut image: bmp::Image) -> bmp::Image {
     return image;
 }
 
-fn draw_square(mut image: bmp::Image, margin_left: u32, margin_right: u32, margin_top: u32, margin_bottom: u32) -> bmp::Image {
-
+fn draw_square(
+    mut image: bmp::Image,
+    margin_left: u32,
+    margin_right: u32,
+    margin_top: u32,
+    margin_bottom: u32,
+) -> bmp::Image {
     for (x, y) in image.coordinates() {
         if (x == margin_left || x == margin_right) && y > margin_top && y < margin_bottom {
             image.set_pixel(y, x, bmp::Pixel::new(255, 255, 255));
@@ -85,9 +102,9 @@ fn avg(i1: bmp::Image, i2: bmp::Image) -> bmp::Image {
         let p1 = i1.get_pixel(x, y);
         let p2 = i2.get_pixel(x, y);
 
-        let r = (p1.r + p2.r) / 2;
-        let g = (p1.g + p2.g) / 2;
-        let b = (p1.b + p2.b) / 2;
+        let r = p1.r / 2 + p2.r / 2;
+        let g = p1.g / 2 + p2.g / 2;
+        let b = p1.b / 2 + p2.b / 2;
 
         image.set_pixel(x, y, bmp::Pixel::new(r, g, b));
     }
@@ -125,9 +142,9 @@ fn main() {
             None => panic!("no second image"),
         },
         "house\n" => draw_house(image),
-        _ =>  {
+        _ => {
             panic!("The operation {op} was not recognized!");
-        },
+        }
     };
 
     image.save(path).expect("This should save correctly.");
